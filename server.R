090cf073120a0a1cb6711c96ad5f955b29b6e33e
koki25ando <basket.koki.bowl@gmail.com>
2020-03-18 23:00:02 +0900

    added renderInfoBox

--- server.R
+++ server.R
@@ -5,7 +5,7 @@ source(file = "global.R",
 
 server = function(input, output) {
   
-  output$showcase_contant = renderReactable({
+  output$showcase_content = renderReactable({
     inFile <- input$file1
     
     if (is.null(inFile))
@@ -22,16 +22,18 @@ server = function(input, output) {
     
     req(input$release_year_slider, input$imdb_rating_slider, input$your_rating_slider)
     
-    df = df %>% 
+    reactable_df = df %>% 
       filter(Year >= input$release_year_slider[1] & Year <= input$release_year_slider[2],
              IMDb_Rating >= input$imdb_rating_slider[1] & IMDb_Rating <= input$imdb_rating_slider[2],
              Your_Rating >= input$your_rating_slider[1] & Your_Rating <= input$your_rating_slider[2])
     
-    reactable(df[, selected_columns],
+    reactable(reactable_df[, selected_columns],
               sortable = TRUE,  showSortable = TRUE
     )
     
   })
+  
+  # -------------------------- Value Box ------------------------------
   
   output$movie_count = renderValueBox({
     #req(input$file1)
@@ -42,12 +44,68 @@ server = function(input, output) {
     movie_count_num = nrow(df)
     
     valueBox(
-      #title = "Movie Count", 
       value = paste0(movie_count_num, " Movies"), 
       subtitle = "Movies You have reviewed", 
       icon = icon("film"),
       color = "yellow"
     )
   })
+  
+  output$average_rating = renderValueBox({
+
+    if (is.null(df))
+      movie_count_num = 0
+    
+    average_rating_value = mean(df$Your_Rating, na.rm = T)
+    
+    valueBox(
+      #title = "Movie Count", 
+      value = round(average_rating_value, digits = 3), 
+      subtitle = "Average Rating Value", 
+      icon = icon("heart"),
+      color = "maroon"
+    )
+  })
+  
+  ## -------------------------- infoBoxoutput
+  
+  output$info_movies_reviewed = renderInfoBox({
+    if (is.null(df))
+      return(NULL)
+    info_review_num = nrow(df)
+    infoBox(title = "Movies Reviewed", value = info_review_num, color = "red",
+            icon = icon("film"))
+  })
+  
+  output$info_aerage_rating = renderInfoBox({
+    if (is.null(df))
+      return(NULL)
+    info_avg_rating = mean(df$Your_Rating, na.rm = T)
+    infoBox(title = "Average Rating", value = info_avg_rating, color = "red",
+            icon = icon("heart"))
+  })
+  
+  output$info_favorite_genre = renderInfoBox({
+    if (is.null(df))
+      return(NULL)
+    info_fav_genre = count(df, Genres) %>% 
+      top_n(1) %>% 
+      pull(Genres) %>% 
+      as.character()
+    infoBox(title = "Favorite Genre", value = as.character(info_fav_genre), color = "red",
+            icon = icon("thumbs-up"))
+  })
+  
+  output$info_favorite_director = renderInfoBox({
+    if (is.null(df))
+      return(NULL)
+    info_fav_director = count(df, Directors) %>% 
+      top_n(1) %>% 
+      pull(Directors) %>% 
+      as.character()
+    infoBox(title = "Favorite Director", value = as.character(info_fav_director), color = "red",
+            icon = icon("user-tie"))
+  })
+  
   
 }
