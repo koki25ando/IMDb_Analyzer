d37a380e9b3e4503fee48c21adf38b63ec71b95e
koki25ando <basket.koki.bowl@gmail.com>
2020-03-18 19:50:55 +0900

    added feature names

--- global.R
+++ global.R
@@ -3,3 +3,6 @@
 ## package loading 
 library(pacman)
 p_load(tidyverse, shiny, shinydashboard, janitor, reactable)
+
+## ------------------------ Data Showcases
+features = c("Title", "Year", "Your_Rating", "IMDb_Rating", "Date_Rated", "Type", "Runtime_Mins", "Genres", "Num_Votes", "Release_Date", "Directors", "URL")
