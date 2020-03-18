8246d372993609dda43bf5b4b1d680dca03b66cf
koki25ando <basket.koki.bowl@gmail.com>
2020-03-18 19:51:14 +0900

    added column_selector

--- ui.R
+++ ui.R
@@ -27,7 +27,8 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Upload Dataset", tabName = "data_upload", icon = icon("upload")),
     menuItem("IMDb Analyzer", tabName = "imdb_analyzer", icon = icon("chart-bar")),
-    menuItem("Information", tabName = "information", icon = icon("info"))
+    menuItem("Information", tabName = "information", icon = icon("info")),
+    menuItem("Documentation", tabName = "documentation", icon = icon("book"))
   )
 )
 
@@ -54,20 +55,34 @@ body <- dashboardBody(
                 icon = icon("question-circle")
               )
             ),
-            
+            infoBoxOutput(outputId = "movie_count"),
+            ),
+            fluidRow(
+              ## ----------------------- Data Column Selector
+            box(
+              width= 3,
+              checkboxGroupInput(
+                input = "column_selector", label = "Columns",
+                choices = features,
+                selected = c("Title", "Your_Rating", "IMDb_Rating", "Release_Date", "Runtime_Mins", "Type")
+                )
+              ),
+
             ## ------------------------ Showcase of Rating dataset
             box(
               title = "Showcase of Rating Dataset",
               label = "rating_showcase",
-              width = 12,
+              width = 9,
               reactableOutput(outputId = "showcase_contant")
-            ))
-            
-            ),
+            )
+          )),
+    
     tabItem(tabName = "imdb_analyzer"
             ),
     tabItem(tabName = "information"
-            )
+            ),
+    tabItem(tabName = "documentation"
+    )
   )
 )
 
