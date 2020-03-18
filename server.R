bfe172bc77ad947f27b6ec73c3916964c64d1f30
koki25ando <basket.koki.bowl@gmail.com>
2020-03-18 19:51:43 +0900

    added column selection

--- server.R
+++ server.R
@@ -13,8 +13,32 @@ server = function(input, output) {
     
     df = read.csv(inFile$datapath, fileEncoding = '', stringsAsFactors = TRUE)
     df = janitor::clean_names(df)
-    reactable(df)
+    names(df) = c("const", "Your_Rating", "Date_Rated", "Title", "URL", "Type", "IMDb_Rating", "Runtime_Mins", "Year", "Genres", 
+                  "Num_Votes", "Release_Date", "Directors")
+    df = df[, features]
     
+    req(input$column_selector)
+    selected_columns = input$column_selector
+    
+    reactable(df[, selected_columns])
+    
+  })
+  
+  output$movie_count = renderValueBox({
+    #req(input$file1)
+    
+    if (is.null(df))
+      movie_count_num = 0
+    
+    movie_count_num = nrow(df)
+    
+    valueBox(
+      #title = "Movie Count", 
+      value = paste0(movie_count_num, " Movies"), 
+      subtitle = "Movies You have reviewed", 
+      icon = icon("film"),
+      color = "yellow"
+    )
   })
   
 }
