126e20d4c18b8a90b521806034fd0a1ed63b0e63
koki25ando <basket.koki.bowl@gmail.com>
2020-03-19 10:28:46 +0900

    added echarts4r package

--- global.R
+++ global.R
@@ -2,7 +2,8 @@
 
 ## package loading 
 library(pacman)
-p_load(tidyverse, shiny, shinydashboard, janitor, reactable)
+p_load(tidyverse, shiny, shinydashboard, janitor, reactable, echarts4r)
 
 ## ------------------------ Data Showcases
-features = c("Title", "Year", "Your_Rating", "IMDb_Rating", "Date_Rated", "Type", "Runtime_Mins", "Genres", "Num_Votes", "Release_Date", "Directors", "URL")
+features = c("Title", "Year", "Your_Rating", "IMDb_Rating", "Date_Rated", "Type", "Runtime_Mins", "Genres", 
+             "Num_Votes", "Release_Date", "Directors", "URL")
