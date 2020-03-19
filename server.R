eda15d6888ee967820cf6659dbb1bd448d960c4e
koki25ando <basket.koki.bowl@gmail.com>
2020-03-19 23:55:48 +0900

    Changes PieChart

--- server.R
+++ server.R
@@ -126,19 +126,11 @@ server = function(input, output) {
                                             "))
   })
   
-  output$genre_pie = renderEcharts4r({
-    
-    genre_list = paste(df$Genres, collapse = ",")
-    splitted_list = strsplit(genre_list, ",")[[1]]
-    splitted_list = sapply(splitted_list, function(x) str_remove_all(x, " "))
-    genre_table = table(splitted_list)
-    data.table(genre_table) %>% 
-      e_charts(splitted_list) %>% 
-      e_pie(N) %>% 
-      e_legend(FALSE) %>% 
-      e_theme("dark")
-    
-  })
+  ## Component: Pie Chart
+  source(file = paste0(COMPONENT_PATH, 'PieChart.R'),
+         local = TRUE,
+         encoding = "UTF-8")
+  
   
   output$viewing_activity = renderEcharts4r({
     runtime_calendar = df %>% 
