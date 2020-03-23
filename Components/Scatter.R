82475a7643566c5501a5d9bab584b083c4fc05ae
koki25ando <basket.koki.bowl@gmail.com>
2020-03-23 15:42:35 +0900

    Added num vote hover info

--- Components/Scatter.R
+++ Components/Scatter.R
@@ -4,8 +4,18 @@ output$rating_scatter = renderEcharts4r({
   df %>% 
     e_charts(IMDb_Rating) %>% 
     e_scatter(Your_Rating, Num_Votes) %>% 
-    e_x_axis(min = min(dat$Your_Rating)) %>% 
+    e_x_axis(min = min(df$Your_Rating)) %>% 
     e_y_axis(min = 1) %>% 
-    e_theme("dark") %>% 
-    e_tooltip()
+    e_theme("dark") %>%
+    e_legend(FALSE) %>% 
+    e_tooltip(formatter = htmlwidgets::JS("
+                                          function(params){
+                                          function kFormatter(num) {
+                                          return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) +
+                                          'k' : Math.sign(num)*Math.abs(num)
+                                          }
+                                          return('Rating: ' + Math.floor(params.value[0]) + 
+                                          '<br />count: ' + params.value[1] +
+                                          '<br />Num Votes: ' + kFormatter(params.value[2])) 
+}"))
 })
