81457052d35433dbbc9e9295df77bfbacb26f0ca
koki25ando <basket.koki.bowl@gmail.com>
2020-03-24 10:56:30 +0900

    Fixed bugs

--- Pages/Analyzer.R
+++ Pages/Analyzer.R
@@ -1,19 +1,38 @@
 fluidPage(fluidRow(
-  infoBoxOutput(outputId = "info_movies_reviewed"),
+  infoBoxOutput(outputId = "info_movies_reviewed") %>% withSpinner(color = "#DD4B39", type = 6, color.background = "white"),
   infoBoxOutput(outputId = "info_aerage_rating"),
   infoBoxOutput(outputId = "info_favorite_genre"),
   infoBoxOutput(outputId = "info_favorite_director")
   ),
 fluidRow(
   box(title = "Rating Score Distribution",
-      echarts4rOutput("rating_histogram")),
+      echarts4rOutput("rating_histogram")) %>% 
+    withSpinner(color = "#DD4B39"),
   box(title = "Movie Genre Pie",
       echarts4rOutput("genre_pie")),
   box(title = "Viewing Activity",
       width = 12, 
-      echarts4rOutput("viewing_activity")),
+      echarts4rOutput("viewing_activity")) %>% 
+    withSpinner(color = "#DD4B39"),
   box(title = "Your Rating vs IMDb Rating",
-      echarts4rOutput("rating_scatter")),
-  box(title = "Monthly Activity",
-      echarts4rOutput("monthly_activity"))
+      echarts4rOutput("rating_scatter")) %>% 
+    withSpinner(color = "#DD4B39"),
+  box(title = "Release Year",
+      echarts4rOutput("release_year")) %>% 
+    withSpinner(color = "#DD4B39"),
+  fluidRow(width = 12,
+           box(title = "Monthly Activity",
+               width = 5,
+               
+               ## ------------- YEAR_MONTH Selector -----------------------
+               uiOutput("year_mon_selector"),
+               ## ---------------------------------------------------------
+               
+               echarts4rOutput("monthly_activity")) %>% 
+             withSpinner(color = "#DD4B39"),
+           
+           box(title = "Monthly Activity Data Table",
+               width = 7,
+               reactableOutput("monthly_activity_table")) %>% 
+             withSpinner(color = "#DD4B39"))
 ))
