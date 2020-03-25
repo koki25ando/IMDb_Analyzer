1b60364df36db4888353ab2474d77cf189e7883e
koki25ando <basket.koki.bowl@gmail.com>
2020-03-25 20:27:44 +0900

    Changed characters

--- Components/PieChart.R
+++ Components/PieChart.R
@@ -22,11 +22,17 @@ output$genre_pie = renderEcharts4r({
     e_pie(N) %>% 
     e_legend(FALSE) %>% 
     e_theme("dark") %>% 
-    e_tooltip(formatter = htmlwidgets::JS("
-                                          function(params){
-                                          return('Genre: ' + params.value[0] + 
-                                          '<br />Count: ' + params.value[1]) 
-}
-  "))
-  
+    e_tooltip()
+})
+
+output$genre_pie_plotly = renderPlotly({
+  genre_df = getGenreTable()
+  p = genre_df %>% 
+    plot_ly(labels = ~splitted_list, values = ~N, type = 'pie', 
+            textposition = "inside") %>%
+    layout(title = 'Movie Genres',
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)) %>% 
+    hide_legend()
+  return(p)
 })
