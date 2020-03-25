d6028089d191c7fee61a008bf979732c1eb3505a
koki25ando <basket.koki.bowl@gmail.com>
2020-03-25 20:28:23 +0900

    Fixed bugs

--- Components/HistogramChart.R
+++ Components/HistogramChart.R
@@ -1,12 +1,13 @@
+# activate reactivity
 output$rating_histogram = renderEcharts4r({
+  df = rowDataImport()
+  
   if (is.null(df))
     return(NULL)
   
   df %>% 
-    # count(Your_Rating) %>% 
-    # head()
-    e_charts() %>% 
-    e_histogram(Your_Rating) %>% 
+    e_chart() %>% 
+    e_histogram(Your_Rating, width = "90%") %>% 
     e_theme("dark") %>% 
     e_x_axis(min = 1) %>% 
     e_tooltip(formatter = htmlwidgets::JS("
