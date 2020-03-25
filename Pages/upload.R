49d63912934ea165f8ea5640092e1990aa10f7bc
koki25ando <basket.koki.bowl@gmail.com>
2020-03-25 20:26:38 +0900

    Changed ui style

--- Pages/upload.R
+++ Pages/upload.R
@@ -13,7 +13,9 @@ tabBox(
     ),
     tabPanel(
       title = "Instruction",
-      icon = icon("question-circle")
+      icon = icon("question-circle"),
+      h5("To analyze your data you need to upload your IMDb data."),
+      h5("See the documentation and follow the setup procedures. Thanks!")
     )),
 infoBoxOutput(outputId = "movie_count"),
 infoBoxOutput(outputId = "average_rating")
@@ -29,7 +31,7 @@ fluidRow(
       selected = c("Title", "Your_Rating", "IMDb_Rating", "Release_Date", "Runtime_Mins", "Type")
     ),
     sliderInput("release_year_slider", label = h5("Release Year Range"), 
-                min = 1920, max = 2020,
+                min = 1910, max = 2020,
                 value = c(1960, 2020)),
     sliderInput("imdb_rating_slider", label = h5("IMDb Rating Range"), 
                 min = 1, max = 10,
@@ -42,6 +44,6 @@ fluidRow(
       title = "Showcase of Rating Dataset",
       label = "rating_showcase",
       width = 9,
-      reactableOutput(outputId = "showcase_content")
+      reactableOutput(outputId = "showcase_content") %>% withSpinner(color = "#DD4B39")
       )
     ))
