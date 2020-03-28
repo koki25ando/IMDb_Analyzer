d552efa3b2a2e56a276dd8b6e9363719424e987f
koki25ando <basket.koki.bowl@gmail.com>
2020-03-28 21:48:46 +0900

    Added new parameteres

--- global.R
+++ global.R
@@ -5,8 +5,20 @@ COMPONENT_PATH = 'Components/'
 PAGE_PATH = 'Pages/'
 
 ## package loading 
-library(pacman)
-p_load(tidyverse, shiny, shinydashboard, htmlwidgets, janitor, reactable, echarts4r, shinycssloaders, DT, data.table, plotly)
+# library(pacman)
+# p_load(tidyverse, shiny, shinydashboard, htmlwidgets, janitor, reactable, echarts4r, shinycssloaders, DT, data.table, plotly)
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(htmlwidgets)
+library(janitor)
+library(reactable)
+library(echarts4r)
+library(shinycssloaders)
+library(DT)
+library(data.table)
+library(plotly)
+
 
 ## ------------------------ Data Showcases
 features = c("Title", "Year", "Your_Rating", "IMDb_Rating", "Date_Rated", "Type", "Runtime_Mins", "Genres", 
@@ -15,6 +27,8 @@ features = c("Title", "Year", "Your_Rating", "IMDb_Rating", "Date_Rated", "Type"
 
 ## --------------- Var Setting ----------------------
 today = Sys.Date()
+today_last_year = today - 365
+this_month_last_year = format(today_last_year + 60, "%Y-%m")
 this_month = format(today, "%Y-%m")
 
 
